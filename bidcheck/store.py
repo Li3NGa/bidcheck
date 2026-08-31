00aa214900e79aed78634b6c2fc40bf7bf9bd5f9
Li3NGa <1753864700@qq.com>
2026-08-31 20:40:58 +0800

    feat: add project listing repository contract

--- bidcheck/store.py
+++ bidcheck/store.py
@@ -6,6 +6,7 @@ from .project import TenderProject
 class ProjectRepository(Protocol):
     def save(self, project: TenderProject) -> None: ...
     def get(self, project_id: str) -> TenderProject | None: ...
+    def list(self) -> list[TenderProject]: ...
 
 @dataclass
 class MemoryProjectRepository:
@@ -13,3 +14,4 @@ class MemoryProjectRepository:
     def __init__(self): self.projects={}
     def save(self, project: TenderProject) -> None: self.projects[project.project_id]=project
     def get(self, project_id: str) -> TenderProject | None: return self.projects.get(project_id)
+    def list(self) -> list[TenderProject]: return list(self.projects.values())
